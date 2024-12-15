5db855c45091195496944d9d99d016e45be938fd
Sam McDonald <s.mcdonald@outlook.com.au>
2024-12-15 22:46:22 +1000

    feat: add comparators to MoneyAmount.php

--- src/ValueObjects/MoneyAmount.php
+++ src/ValueObjects/MoneyAmount.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Signal\Core\ValueObjects;
 
-use Signal\Core\ValueObjects\Contracts\FloatValueObject;
+use Signal\Core\ValueObjects\Contracts\NumericValueObject;
 
 /**
  * MoneyAmount.
@@ -15,11 +15,26 @@ use Signal\Core\ValueObjects\Contracts\FloatValueObject;
  *
  * @author s.mcdonald@outlook.com.au
  */
-final class MoneyAmount extends FloatValueObject
+final class MoneyAmount extends NumericValueObject
 {
     public function getValue(): float
     {
-        return parent::getValue();
+        return (float) parent::getValue();
+    }
+
+    public function greaterThan(MoneyAmount $other): bool
+    {
+        return $this->getValue() > $other->getValue();
+    }
+
+    public function lessThan(MoneyAmount $other): bool
+    {
+        return $this->getValue() < $other->getValue();
+    }
+
+    public function equalTo(MoneyAmount $other): bool
+    {
+        return $this->getValue() === $other->getValue();
     }
 
     protected function checkBoundary(): void
