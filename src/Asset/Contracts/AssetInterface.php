53e4f78e2d7b7a53efac693c8e2106f0b7fa3217
Sam McDonald <s.mcdonald@outlook.com.au>
2024-12-31 17:37:04 +1000

    style: cs fixer clean code

--- src/Asset/Contracts/AssetInterface.php
+++ src/Asset/Contracts/AssetInterface.php
@@ -4,9 +4,9 @@ declare(strict_types=1);
 
 namespace Signal\Core\Asset\Contracts;
 
+use Signal\Core\Asset\Contracts\Components\ProvidesAssetTypeInterface;
 use Signal\Core\ValueObjects\AssetShortName;
 use Signal\Core\ValueObjects\AssetSymbol;
-use Signal\Core\Asset\Contracts\Components\ProvidesAssetTypeInterface;
 
 /**
  * @author s.mcdonald@outlook.com.au
