fdaa64fee70461f7c0f5df346faaf41162d4fe6b
Sam McDonald <s.mcdonald@outlook.com.au>
2024-12-31 17:34:51 +1000

    updated: AssetInterface to use Provider Components

--- src/Asset/Contracts/AssetInterface.php
+++ src/Asset/Contracts/AssetInterface.php
@@ -4,17 +4,15 @@ declare(strict_types=1);
 
 namespace Signal\Core\Asset\Contracts;
 
-use Signal\Core\Enums\AssetType;
 use Signal\Core\ValueObjects\AssetShortName;
 use Signal\Core\ValueObjects\AssetSymbol;
+use Signal\Core\Asset\Contracts\Components\ProvidesAssetTypeInterface;
 
 /**
  * @author s.mcdonald@outlook.com.au
  */
-interface AssetInterface
+interface AssetInterface extends ProvidesAssetTypeInterface
 {
-    public function getType(): AssetType;
-
     public function getSymbol(): AssetSymbol;
 
     public function getShortName(): AssetShortName;
